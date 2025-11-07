25b0565225bb47a5b9d577d34371ea37695d1a5e
Septem Li <1216949+septemhill@users.noreply.github.com>
2025-11-07 14:33:22 +0800

    init commit

---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "postgres" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('postgres.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from postgres!');
+	});
+
+	context.subscriptions.push(disposable);
+
+	const showTreeView = vscode.commands.registerCommand('postgres.showTreeView', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'postgresView',
+			'Postgres',
+			vscode.ViewColumn.One,
+			{}
+		);
+
+		panel.webview.html = `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Postgres</title>
+			</head>
+			<body>
+				<h1>Hello from Postgres!</h1>
+			</body>
+			</html>`;
+	});
+
+	context.subscriptions.push(showTreeView);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
